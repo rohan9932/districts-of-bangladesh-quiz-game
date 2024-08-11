8d9432e2079bba41348db0b3a3022ff93f837572
Rohan Rahim <rohanrahim024@gmail.com>
2024-08-11 19:35:38 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,37 @@
+from turtle import Turtle, Screen
+import pandas as pd
+
+screen = Screen()
+screen.title("Districts of Bangladesh")
+image = "64Districts.gif"
+screen.bgpic(image)
+screen.setup(605,832)
+
+# Data Processing
+districts_data = pd.read_csv("64Divisions.csv")
+all_districts = districts_data.district.tolist()
+guessed_districts = []
+
+while len(guessed_districts) < 64:
+    answer_district = screen.textinput(title=f"{len(guessed_districts)}/64 Districts Correct",
+                                       prompt="What's another district's name?").title()
+
+    if answer_district == "Exit":
+        missing_districts = []
+        for district in all_districts:
+            if district not in guessed_districts:
+                missing_districts.append(district)
+        data = pd.DataFrame(missing_districts)
+        data.to_csv("districts_to_learn.csv")
+        break
+
+    if answer_district in all_districts:
+        guessed_districts.append(answer_district)
+        # move the name of the state to the location
+        t = Turtle()
+        t.hideturtle()
+        t.penup()
+        district_data = districts_data[districts_data.district == answer_district]
+        t.goto(district_data.x.item(), district_data.y.item())
+        t.write(answer_district)
+
